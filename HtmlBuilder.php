57b72d5c84fd57857f83b789f5f44059688c4a81
digim2010 <72142669+digim2010@users.noreply.github.com>
2020-09-30 18:57:35 +0530

    Update HtmlBuilder.php

--- HtmlBuilder.php
+++ HtmlBuilder.php
@@ -15,7 +15,7 @@ class HtmlBuilder {
 	protected $url;
 
 	/**
-	 * Create a new HTML builder instance.
+	 * Create a new HTML builder instance of this code.
 	 *
 	 * @param  \Illuminate\Routing\UrlGenerator  $url
 	 * @return void
